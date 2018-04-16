8f92989930c7eccc6ee569d2a48feca27d5cada2
Maximillian Silva <maxsilvauk@gmail.com>
2018-04-16 22:59:11 +0100

    Laravel 5.6 + .gitignore updates

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        // Fixes this: Versions => Laravel 5.4: Specified key was too long error
+        Schema::defaultStringLength(191);
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
