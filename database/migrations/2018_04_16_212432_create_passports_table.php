0f082478c147ceb255df06d97ceaf6572f16c081
Max Silva <maxsilvauk@gmail.com>
2018-04-26 20:28:36 +0100

    Updated number type to string so can enter > value

--- database/migrations/2018_04_16_212432_create_passports_table.php
+++ database/migrations/2018_04_16_212432_create_passports_table.php
@@ -18,7 +18,7 @@ class CreatePassportsTable extends Migration
             $table->string('name');
             $table->integer('date');
             $table->string('email')->unique();
-            $table->integer('number');
+            $table->string('number');
             $table->string('office');
             $table->string('filename');
             $table->timestamps();
